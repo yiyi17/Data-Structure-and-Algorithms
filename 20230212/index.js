5ae2695556652359a5ae9fe457eb5f32ee9dca51
lei <25927480+yiyi17@users.noreply.github.com>
2023-02-12 18:07:23 +0800

    feat: 金币之和求硬币数

---
+++ 20230212/index.js
@@ -0,0 +1,23 @@
+// 不同硬币面额 coins，求总 amount 需要的最少硬币数
+// dp[0] = 0 金额为零时不需要硬币
+// dp[n] = min(dp[n],dp[n-coin1] + 1，dp[n-coin2],...)  金额为n时，硬币数等于(n-coin)+1中所需硬币最少的组合
+
+
+function caculate(amount,coins) {
+  let dp = new Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+
+  for(let i = 0; i <= amount; i++) {
+    for(let coin of coins) {
+      if(i - coin >= 0) {
+
+        dp[i] = Math.min(dp[i], dp[i-coin] + 1)
+      }
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount]
+
+}
+
+console.log(caculate(7, [4,5,6]));
--- README.md
+++ README.md
@@ -27,9 +27,13 @@
 - [深拷贝](20221219/deepclone.js)
 
 - [串联同步执行并发请求](20221220/recorder.ts)
+  
+- [不同硬币面额 coins，求总 amount 需要的最少硬币数](20230212/index.js)
 
 
 ## 参考资料
 
 [JavaScript 数据结构与算法学习笔记（专辑）
 ](https://xpoet.cn/2020/07/JavaScript%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E4%B8%8E%E7%AE%97%E6%B3%95%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0%EF%BC%88%E4%B8%93%E8%BE%91%EF%BC%89/)
+
+- [「2021」高频前端面试题汇总之手写代码篇](https://juejin.cn/post/6946136940164939813)
