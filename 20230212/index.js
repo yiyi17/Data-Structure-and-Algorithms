fe64bf88c1d36f28f523d0cfd4769072636b50a7
yiyi17 <lileibao13@163.com>
2023-02-14 13:09:46 +0800

    Update index.js

--- 20230212/index.js
+++ 20230212/index.js
@@ -19,4 +19,29 @@ function caculate(amount,coins) {
 
 }
 
+
+
 console.log(caculate(7, [2,4,5,6]));
+
+
+const allMoney = [100, 50, 20, 5, 1];  // 表示我们手上有的面值
+function changeMoney(n, allMoney) {
+  const length = allMoney.length;
+  const result = [];    // 存储结果的数组，每项表示对应面值的张数
+  for(let i = 0; i < length; i++) {
+    if(n >= allMoney[i]) {
+      // 如果需要找的钱比面值大，那就可以找，除一下看看能找几张
+      result[i] = parseInt(n / allMoney[i]);
+      n = n - result[i] * allMoney[i];   // 更新剩下需要找的钱
+    } else {
+      // 否则不能找
+      result[i] = 0;
+    }
+  }
+  
+  return result;
+}
+
+const result = changeMoney(126, allMoney);
+console.log(result);   // [1, 0, 1, 1, 1]
+
