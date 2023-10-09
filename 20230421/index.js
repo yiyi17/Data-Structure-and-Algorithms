dd3441637a9788353366fccdcb10ea4a5222d39c
yiyi17 <ielyiyi17@gmail.com>
2023-10-09 16:12:33 +0800

    feat: 排序

--- 20230421/index.js
+++ 20230421/index.js
@@ -9,8 +9,27 @@ function quickSort(arr) {
   let right = [];
 
   for(let i = 0; i < arr.length; i++) {
-    if(arr[i] < pivot) left.push(arr[i]);
+    if(arr[i] <= pivot) left.push(arr[i]);
     else if(arr[i] > pivot) right.push(arr[i]);
   }
   return quickSort(left).concat(pivot, quickSort(right));
+}
+
+/**
+ * Sorts an array in ascending order using the insertion sort algorithm.
+ *
+ * @param {Array} arr - The array to be sorted.
+ * @return {Array} - The sorted array.
+ */
+const insertionSort = function(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
 }
