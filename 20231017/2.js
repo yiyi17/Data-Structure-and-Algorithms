6847c3d11892436709c1a7120504bbcd8bc19b84
yiyi17 <lileibao13@163.com>
2023-10-17 11:53:40 +0800

    feat: 复杂度

---
+++ 20231017/2.js
@@ -0,0 +1,20 @@
+
+// 在严格模式下的输出
+let func = function () {
+  this.value = 1;
+};
+let obj = new func();
+func.prototype.value = 2;
+let value = 3;
+func();
+
+console.log(value); // 3
+console.log(obj.value);// 1
+
+
+
+/**
+ * 前端严格模式、非严格模式的区别
+ */
+
+// 空间复杂度与时间复杂度
