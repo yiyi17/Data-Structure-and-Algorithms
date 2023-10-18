c544a35cffe37bc50372d1f7ce3e2ec79c459e3f
yiyi17 <lileibao13@163.com>
2023-10-18 22:10:51 +0800

    feat: o1 数组处理

--- 20231017/index2.js
+++ 20231017/index2.js
@@ -3,12 +3,15 @@
 function handleArr(arr) {
   const len = arr.length;
   for (let i = 0; i < len; i++) {
-    if(arr[i] === 0) {
+    if (arr[i] === 0 && arr[i + 1] !== 0) {
+      arr.splice(i, 1);
       arr.push(0);
-      arr[i] = undefined
+    }else if (arr[i] ===0 && arr[i + 1] ===0){
+      arr.splice(i, 2);
+      arr =  arr.concat([0,0]);
     }
   }
-  return arr.filter(item => item !== undefined)
+  return arr
 }
 
-console.log(handleArr([3,0,0,12,0,1]));
+console.log(handleArr([3,0,0,12,0,1,0]));
