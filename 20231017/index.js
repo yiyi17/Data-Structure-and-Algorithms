049d1fd5b9fc1744fc1101fa010516357f9634af
baolilei <baolilei@zhihu.com>
2023-10-19 10:12:32 +0800

    feat: while 循环

--- 20231017/index.js
+++ 20231017/index.js
@@ -3,15 +3,28 @@
 
 function sum(n) {
   let sum = 0
-
   for (let i = 1; i <= n; i++) {
-    if(n%3 === 0 || n%5 === 0 || n%7 === 0) {
-      console.log(i);
+    if(i%3 === 0 || i%5 === 0 || i%7 === 0) {
+      console.log('1----',i);
       sum += i
     }
   }
-
   return sum
 }
 
+
+function sumWhile(n) {
+  let sum = 0
+
+  while(n > 0) {
+    if(n%3 === 0 || n%5 === 0 || n%7 === 0) {
+      console.log('2----',n);
+      sum += n
+    }
+    n--
+  }
+  return sum
+}
+
+// console.log(sumWhile(7));// 21
 console.log(sum(7));// 21
