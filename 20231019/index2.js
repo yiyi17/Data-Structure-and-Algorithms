0dbab14016b5307b0ed49652ebf4fc6ca419a1c8
yiyi17 <lileibao13@163.com>
2023-10-26 22:33:52 +0800

    feat: 同元组求积

---
+++ 20231019/index2.js
@@ -0,0 +1,26 @@
+// 同积元祖
+
+function handleArr(arr) {
+ let count = 0
+  
+ let left = 0
+ let right = arr.length -1
+
+ while(left+2 < right) {
+  const res = arr[left] * arr[right]
+  for(let i = left + 1; i < right; i++) {
+    if(arr.includes(res/arr[i])) {
+      count+=8
+      break
+    }
+  }    
+  left++
+ }
+
+
+  console.log(count);
+  return count
+}
+
+handleArr([2,3,4,6])//8
+handleArr([1,2,4,5,10])//16
