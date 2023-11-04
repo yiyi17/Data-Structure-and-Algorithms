49738fed7484ec54638a0df80d7b0847f9eccc43
yiyi17 <lileibao13@163.com>
2023-11-04 22:18:48 +0800

    feat: 合并数组

---
+++ 20231104/index2.js
@@ -0,0 +1,24 @@
+// 合并两个数组，输入的两个数组是有序的，升序，合并完成后仍然有序
+
+var merge = function(nums1, m, nums2, n) {
+  let p1 = m - 1, p2 = n - 1
+  let cur
+  let tail = m + n - 1
+
+  while(p1>=0 || p2>=0) {
+    if(p1 === -1) {
+      cur = nums2[p2--]
+    }else if(p2 === -1) {
+      cur = nums1[p1--]
+    }else if(nums1[p1] > nums1[p2]) {
+      cur = nums1[p1--]
+    }else {
+      cur = nums2[p2--]
+    }
+    nums1[tail--] = cur
+  }
+  nums1
+};
+
+console.log(merge([1,2,3,0,0,0],3, [2,5,6],3));
+
