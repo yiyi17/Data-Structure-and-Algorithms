4098e2713a913794b1de953c51a7a52962805a20
yiyi17 <ielyiyi17@gmail.com>
2023-11-16 18:07:20 +0800

    feat: array

---
+++ 2021/array/index.js
@@ -0,0 +1,17 @@
+let arr  = []
+
+arr.push(1,1,2)
+
+arr
+
+const b = arr.keys()
+
+let c = b.next()
+
+c
+
+const boolean =  arr.every((item) => item > 1)
+const boolean2 =  arr.some((item) => item > 1)
+
+boolean
+boolean2
