af2c5e55dce7f3ec0d699139857328e41f77c68d
yiyi17 <lileibao13@163.com>
2023-12-29 23:50:41 +0800

    feat: 二分查找

--- 20231229/index5.js
+++ 20231229/index5.js
@@ -27,11 +27,11 @@ var findPeakElement = function(nums) {
   let left = 0, right = n - 1, ans = -1;
   while (left <= right) {
       const mid = Math.floor((left + right) / 2);
-      if (compare(nums, mid - 1, mid) < 0 && compare(nums, mid, mid + 1) > 0) {
+      if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
           ans = mid;
           break;
       }
-      if (compare(nums, mid, mid + 1) < 0) {
+      if (nums[mid] < nums[mid+1]) {
           left = mid + 1;
       } else {
           right = mid - 1;
@@ -41,28 +41,6 @@ var findPeakElement = function(nums) {
   return ans;
 }
 
-// 辅助函数，输入下标 i，返回一个二元组 (0/1, nums[i])
-// 方便处理 nums[-1] 以及 nums[n] 的边界情况
-const get = (nums, idx) => {
-  if (idx === -1 || idx === nums.length) {
-      return [0, 0];
-  }
-  return [1, nums[idx]];
-}
-
-const compare = (nums, idx1, idx2) => {
-  const num1 = get(nums, idx1);
-  const num2 = get(nums, idx2);
-  num1
-  num2
-  if (num1[0] !== num2[0]) {
-      return num1[0] > num2[0] ? 1 : -1;
-  }
-  if (num1[1] === num2[1]) {
-      return 0;
-  }
-  return num1[1] > num2[1] ? 1 : -1;
-}
 
 
 findPeakElement([1,2,1,3,5,6,4])
