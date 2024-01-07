0af6731741f63c55051d355d033fd96514c67219
yiyi17 <lileibao13@163.com>
2024-01-07 20:29:17 +0800

    fix: 搜索旋转排序数组

--- 20240104/index.js
+++ 20240104/index.js
@@ -36,24 +36,23 @@ var search = function(nums, target) {
         // 如果中间数小于最右边数，则右半段是有序的
         // 如果中间数大于最右边数，则左半段是有序的
         if(nums[mid] < nums[right]) {
-            if(target > nums[mid] && target < nums[end]) {
+            if(target > nums[mid] && target <= nums[right]) {
                 left = mid + 1
             }else {
                 right = mid - 1
             }
         }else {
-            if(target > nums[left] && target < nums[mid]) {
+            if(target >= nums[left] && target < nums[mid]) {
                 right = mid - 1
             }else {
                 left = mid + 1
             }
         }
     }
-
     return -1
 }
-
-
+const nums =[4,5,6,7,0,1,2], target = 0
+console.log(search(nums, target));
 
 var findMin = function(nums) {
     if(!nums.length) return null
@@ -62,8 +61,10 @@ var findMin = function(nums) {
     if(nums[right] > nums[left]) return nums[0]
     while(left <= right) {
         mid = left + Math.floor((right - left)/2)
+        
         mid
         
+
         if(nums[mid] > nums[mid+1]) {
             return nums[mid+1]
         }
@@ -83,9 +84,9 @@ var findMin = function(nums) {
 } 
 
 
-const nums = [7,0,1,2,3,4,5,6], target = 0
-console.log(search(nums, target));
+
 
 console.log(findMin(nums));
+
 
 // 而乱序的条件则是：first element > last element
