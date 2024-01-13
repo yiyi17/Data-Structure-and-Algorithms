e8c32a634d0ba6f300c50679612466dc8c970cad
yiyi17 <lileibao13@163.com>
2024-01-13 20:26:13 +0800

    feat: ffmpeg 转 mp3 wav

---
+++ 20240112/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const child_process = require('child_process')
+
+/**
+ * @description 转换音频文件
+ */
+function convertAudio(inputFile, outputFile) {
+  // 利用 ffmpeg 来转换，需要安装一下（mac：brew install ffmpeg）
+  const ffmpegCommand = `ffmpeg -i ${inputFile} ${outputFile}`
+
+  const cp = child_process.exec(ffmpegCommand, (error, stdout, stderr) => {
+    if(error) {
+      console.log(error)
+    }else {
+      console.log('success');
+    }
+  })
+}
+
+
+// 文件地址
+// const inputFile = 'xuehe.ncm'
+const inputFile = '萨顶顶-问灵.mp3'
+// 转出文件
+const outputFile = '萨顶顶-问灵.wav'
+
+convertAudio(inputFile, outputFile)
+
+
+
