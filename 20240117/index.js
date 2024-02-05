30bcbe6dc9ca02e903e88cfa963f472c161d55f2
yiyi17 <ielyiyi17@gmail.com>
2024-02-05 11:09:06 +0800

    feat: 内存泄露

--- 20240117/index.js
+++ 20240117/index.js
@@ -1,19 +1,24 @@
 const http = require('http')
 
 // 编写一个内存溢出的函数
-// const arr = []
-// function list() {
-//   for(let i = 0; i < 1000; i++) {
-//     arr.push(100000000000000)
-//   }
-// }
+const arr = []
+function list() {
+  for(let i = 0; i < 1000; i++) {
+    arr.push(100000000000000)
+  }
+}
 
 // 监听内存变化
 
 // 启动一个服务看下
 
+
 http.createServer((req, res) => {
-  res.write('hello world')
+  list()
+  console.log(arr.length);
+  const memoryUsage = process.memoryUsage()
+  console.log(memoryUsage);
+  res.write(arr.toString())
   res.end()
 })
 .listen(3000, () => {
