d57bd334f00c7f284aa05e803692725b8b1efaf5
yiyi17 <ielyiyi17@gmail.com>
2024-02-20 17:21:02 +0800

    feat: 迭代与递归的转换

--- 20240220/Index3.js
+++ 20240220/Index3.js
@@ -26,16 +26,40 @@ let company = {
   }
 };
 
+// function totalSalary(department) {
+//   if (Array.isArray(department)) {
+//     return department.reduce((prev, current) => prev + current.salary, 0)
+//   } else {
+//     let sum = 0
+//     for (let subdep of Object.values(department)) {
+//       sum += totalSalary(subdep)
+//     }
+//     return sum
+//   }
+// }
+
 function totalSalary(department) {
-  if (Array.isArray(department)) {
-    return department.reduce((prev, current) => prev + current.salary, 0)
-  } else {
-    let sum = 0
-    for (let subdep of Object.values(department)) {
-      sum += totalSalary(subdep)
-    }
-    return sum
+
+  function calculateSalary(department) {
+      return department.reduce((prev,current) => prev + current.salary, 0)
   }
-}
+
+  let sum = 0
+  const values = Object.values(department)
+  let queue = [...values]
+
+  while(queue.length > 0){
+    const current = queue.shift()
+    if(Array.isArray(current)){
+      sum += calculateSalary(current)
+    }else {
+      queue.push(...Object.values(current))
+    }
+  }
+  console.log(sum);
+
+  return sum
+  }
+
 console.log(totalSalary(company)); // 7700
 
