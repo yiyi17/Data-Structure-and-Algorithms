4884a8ffb775e607f7b66e61b0972cf17922f349
yiyi17 <lileibao13@163.com>
2024-02-28 22:14:15 +0800

    feat: 回文子串

---
+++ 20240228/index.js
@@ -0,0 +1,19 @@
+// 回文子串
+
+// 使用双指针
+
+function fn(str) {
+  let left = 0, right = str.length - 1
+  while(left < right) {
+    if(str[left] === str[right]) {
+      left++
+      right--
+    } else {
+      console.log('不是回文子串')
+      return false
+    }
+  }
+  return true
+}
+
+console.log(fn('abba'));
