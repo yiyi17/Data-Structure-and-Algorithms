8e92ec349f34351a50a326159283bbe978ff31df
yiyi17 <lileibao13@163.com>
2024-05-15 22:51:23 +0800

    fix: 1

--- 2023/20231029/reg.js
+++ 2023/20231029/reg.js
@@ -2,16 +2,13 @@
 // ? 表示匹配前面的子表达式零次或一次。例如，a? 可以匹配空字符串或单个 a 字符。
 // 捕获组 (.*?)
 const regex = /\+(.*?)\+/;
-
 const string = 'ab+e+dg';
-
 const match = string.match(regex);
-
 if (match) {
   match;
   console.log(match[1]); // 输出: e
 }
 
-const str = '00ddd55';
-
-console.log(str.match(/d+(.*?)d+/));
+const str = '00+dd1d+55';
+const match2 = str.match(/d+(.*?)d+/)
+console.log(match2);
